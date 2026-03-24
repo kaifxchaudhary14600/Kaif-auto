ed8cc7b0b63791167eb9321eedbd4c23d46d6e71
Itxxwasi <somkerboy061@gmail.com>
2026-03-24 18:23:15 +0500

    fix: remove heroku.yml, use nodejs buildpack

--- wasi.js
+++ wasi.js
@@ -2,6 +2,5 @@ require('dotenv').config();
 
 module.exports = {
     sessionId: process.env.SESSION_ID || 'jiftxgtgtbo',
-    mongoDbUrl: process.env.MONGODB_URI || process.env.MONGODB_URL || '
-
-
+    mongoDbUrl: process.env.MONGODB_URI || process.env.MONGODB_URL || '',
+}
